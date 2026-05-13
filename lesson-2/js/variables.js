6535e73f20f6c43e759baf5402dc5c493058c55b
shiv-coder <shivi359@gmail.com>
2026-05-13 10:05:22 -0400

    com pleted file variables

--- lesson-2/js/variables.js
+++ lesson-2/js/variables.js
@@ -1,25 +1,49 @@
 /* STEP 1: Declare variables
 		Use a-z, 0-9, A-Z and underscores, avoiding reserved names
 		All program variables are generally declared at the beginning */
-let x = 10;
+		let myName;
+		let myCourse;
+		let string;
+		let para = document.querySelector('p');
 
 		/* STEP 2: Initialize variables
 		Don't confuse a variable with no value with one that doesn't exist at all */
-
+		myName = 'Shivali Dhaka';
+		myCourse = 'Client-Side JavaScript';
 
 		/* STEP 3: Updating variables
 		Once a variable has been declared, this can be done any time */
+		myName = 'Shivi';
+		myCourse = 'CJS';
+		string = myName + ' ' + myCourse;
+		para.textContent = string;
 
 
 		// STEP 4: Variable types (use typeof in the console)
 		// Numbers
+		let numCourse = 6;
+		para.textContent = numCourse;
+		console.log(typeof(numCourse));
 
 		// Strings (use '' or "")
+		let stringCourse = 'Client-Side JS';
+		para.textContent = stringCourse;
 
 		// Booleans
+		let isAmAlive = true;
+		let compare = 5 > 2;
+		para.textContent = isAmAlive;
+		console.log('The result of the comparison(5>2) is: ' + compare);
 
 		// Arrays
+		let myArrays = ['Sarah','Steeve','Kasib'];
+		para.textContent = myArrays[1];
 		
 		// Objects
+		let myDog = {fName : 'Simba',
+					breed : 'Staffy',
+					color : 'brown'
+		}
+		console.log(myDog.color);
 
 		// STEP 5: Typing (JavaScript is a loosely-typed language)
