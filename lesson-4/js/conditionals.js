f46292941e5a0bcf63334f41b81e7448ae56f3fc
shiv-coder <shivi359@gmail.com>
2026-05-27 10:09:45 -0400

    completed switch

--- lesson-4/js/conditionals.js
+++ lesson-4/js/conditionals.js
@@ -101,20 +101,28 @@ windSpeed.addEventListener('change', getWindSpeed);
 function getWindSpeed() {
 let speed = Number(windSpeed.value);
 // STEP 5a: Build the SWITCH code block
-
+switch (speed)
 	// STEP 5b: Build out four cases, followed by a default
-
+	{
+		case 100:
 		// 100 km/h
 		windComment.textContent = speed + 'km/h - Holy schmoly! Hold on to your hat!';
+		break;
 
+		case 75:
 		// 75 km/h
 		windComment.textContent = speed + 'km/h - It\'s a-howling out there!';
+		break;
 
+		case 50:
 		// 50 km/h
 		windComment.textContent = speed + 'km/h - Quite gusty out there, today.';
-
+		break;
+		
+		default:
 		// 25 km/h
 		windComment.textContent = speed + 'km/h - A wee bit breezy.';
+	}
 
 } // End of getWindSpeed() function
 
